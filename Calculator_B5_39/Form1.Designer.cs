92ce61fdeeb4f74b89bde747524076089590e226
Varshita <52200093+varshi12345@users.noreply.github.com>
2021-09-20 15:01:37 +0530

    Add files via upload

---
+++ Calculator_B5_39/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿
+namespace Calculator_B5_39
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.bC = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.subtract = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.sqrt = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.dot = new System.Windows.Forms.Button();
+            this.bCE = new System.Windows.Forms.Button();
+            this.tB_output = new System.Windows.Forms.TextBox();
+            this.label_operation_string = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(25, 239);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(51, 50);
+            this.b1.TabIndex = 0;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b8.Location = new System.Drawing.Point(82, 127);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(51, 50);
+            this.b8.TabIndex = 1;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b9.Location = new System.Drawing.Point(139, 127);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(51, 50);
+            this.b9.TabIndex = 2;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // bC
+            // 
+            this.bC.BackColor = System.Drawing.Color.Pink;
+            this.bC.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bC.Location = new System.Drawing.Point(253, 127);
+            this.bC.Name = "bC";
+            this.bC.Size = new System.Drawing.Size(51, 50);
+            this.bC.TabIndex = 3;
+            this.bC.Text = "C";
+            this.bC.UseVisualStyleBackColor = false;
+            this.bC.Click += new System.EventHandler(this.bC_Click);
+            // 
+            // add
+            // 
+            this.add.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add.Location = new System.Drawing.Point(196, 127);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(51, 50);
+            this.add.TabIndex = 4;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.op_Click);
+            // 
+            // equal
+            // 
+            this.equal.BackColor = System.Drawing.Color.LightGreen;
+            this.equal.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equal.Location = new System.Drawing.Point(253, 239);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(51, 106);
+            this.equal.TabIndex = 9;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = false;
+            this.equal.Click += new System.EventHandler(this.equal_Click);
+            // 
+            // subtract
+            // 
+            this.subtract.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtract.Location = new System.Drawing.Point(196, 183);
+            this.subtract.Name = "subtract";
+            this.subtract.Size = new System.Drawing.Size(51, 50);
+            this.subtract.TabIndex = 8;
+            this.subtract.Text = "-";
+            this.subtract.UseVisualStyleBackColor = true;
+            this.subtract.Click += new System.EventHandler(this.op_Click);
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b6.Location = new System.Drawing.Point(139, 183);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(51, 50);
+            this.b6.TabIndex = 7;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b5.Location = new System.Drawing.Point(82, 183);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(51, 50);
+            this.b5.TabIndex = 6;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b4.Location = new System.Drawing.Point(25, 183);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(51, 50);
+            this.b4.TabIndex = 5;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiply.Location = new System.Drawing.Point(196, 239);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(51, 50);
+            this.multiply.TabIndex = 13;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.op_Click);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(139, 239);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(51, 50);
+            this.b3.TabIndex = 12;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(82, 239);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(51, 50);
+            this.b2.TabIndex = 11;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b7.Location = new System.Drawing.Point(25, 127);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(51, 50);
+            this.b7.TabIndex = 10;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // divide
+            // 
+            this.divide.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divide.Location = new System.Drawing.Point(196, 295);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(51, 50);
+            this.divide.TabIndex = 18;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.op_Click);
+            // 
+            // sqrt
+            // 
+            this.sqrt.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sqrt.Location = new System.Drawing.Point(139, 295);
+            this.sqrt.Name = "sqrt";
+            this.sqrt.Size = new System.Drawing.Size(51, 50);
+            this.sqrt.TabIndex = 17;
+            this.sqrt.Text = "-/";
+            this.sqrt.UseVisualStyleBackColor = true;
+            this.sqrt.Click += new System.EventHandler(this.op_Click);
+            // 
+            // b0
+            // 
+            this.b0.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b0.Location = new System.Drawing.Point(82, 295);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(51, 50);
+            this.b0.TabIndex = 16;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // dot
+            // 
+            this.dot.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dot.Location = new System.Drawing.Point(25, 295);
+            this.dot.Name = "dot";
+            this.dot.Size = new System.Drawing.Size(51, 50);
+            this.dot.TabIndex = 15;
+            this.dot.Text = ".";
+            this.dot.UseVisualStyleBackColor = true;
+            this.dot.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // bCE
+            // 
+            this.bCE.BackColor = System.Drawing.Color.Pink;
+            this.bCE.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCE.Location = new System.Drawing.Point(253, 183);
+            this.bCE.Name = "bCE";
+            this.bCE.Size = new System.Drawing.Size(51, 50);
+            this.bCE.TabIndex = 19;
+            this.bCE.Text = "CE";
+            this.bCE.UseVisualStyleBackColor = false;
+            this.bCE.Click += new System.EventHandler(this.bCE_Click);
+            // 
+            // tB_output
+            // 
+            this.tB_output.BackColor = System.Drawing.Color.White;
+            this.tB_output.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tB_output.Location = new System.Drawing.Point(25, 79);
+            this.tB_output.Name = "tB_output";
+            this.tB_output.Size = new System.Drawing.Size(278, 40);
+            this.tB_output.TabIndex = 20;
+            this.tB_output.Text = "0";
+            this.tB_output.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label_operation_string
+            // 
+            this.label_operation_string.AutoSize = true;
+            this.label_operation_string.BackColor = System.Drawing.Color.White;
+            this.label_operation_string.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_operation_string.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label_operation_string.Location = new System.Drawing.Point(19, 44);
+            this.label_operation_string.Name = "label_operation_string";
+            this.label_operation_string.Size = new System.Drawing.Size(0, 32);
+            this.label_operation_string.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 371);
+            this.Controls.Add(this.label_operation_string);
+            this.Controls.Add(this.tB_output);
+            this.Controls.Add(this.bCE);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.sqrt);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.dot);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.subtract);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.bC);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator_B5_39";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button bC;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button equal;
+        private System.Windows.Forms.Button subtract;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button sqrt;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button dot;
+        private System.Windows.Forms.Button bCE;
+        private System.Windows.Forms.TextBox tB_output;
+        private System.Windows.Forms.Label label_operation_string;
+    }
+}
+
---
+++ Calculator_B5_39/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_B5_39
+{
+    public partial class Form1 : Form
+    {
+        Double output = 0;
+        String operation = "";
+        bool operator_given = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            if ((tB_output.Text == "0") || (operator_given))
+                tB_output.Clear();
+            operator_given = false;
+            Button button = (Button)sender;
+            if (button.Text == ".")
+            {
+                if (!tB_output.Text.Contains("."))
+                {
+                    tB_output.Text = tB_output.Text + button.Text;
+                }
+            }
+            else
+            {
+                tB_output.Text = tB_output.Text + button.Text;
+            }
+        }
+
+        private void op_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (output != 0)
+            {
+                equal.PerformClick();
+                operation = button.Text;
+                output = Double.Parse(tB_output.Text);
+                label_operation_string.Text = output + " " + operation;
+                operator_given = true;
+
+            }
+            if(button.Text=="-/")
+            {
+                operation = button.Text;
+                output = Double.Parse(tB_output.Text);
+                label_operation_string.Text = "-/(" + output + ")";
+                operator_given = true;
+            }
+            else
+            {
+                operation = button.Text;
+                output = Double.Parse(tB_output.Text);
+                label_operation_string.Text = output + " " + operation;
+                operator_given = true;
+            }
+        }
+
+        private void bC_Click(object sender, EventArgs e)
+        {
+            tB_output.Text = "0";
+            output = 0;
+        }
+
+        private void bCE_Click(object sender, EventArgs e)
+        {
+            tB_output.Text = "0";
+        }
+
+        private void equal_Click(object sender, EventArgs e)
+        {
+            switch (operation)
+            {
+                case "+":
+                    tB_output.Text = (output + Double.Parse(tB_output.Text)).ToString();
+                    break;
+                case "-":
+                    tB_output.Text = (output - Double.Parse(tB_output.Text)).ToString();
+                    break;
+                case "*":
+                    tB_output.Text = (output * Double.Parse(tB_output.Text)).ToString();
+                    break;
+                case "/":
+                    tB_output.Text = (output / Double.Parse(tB_output.Text)).ToString();
+                    break;
+                case "-/":
+                    tB_output.Text = (Math.Sqrt(output)).ToString();
+                    break;
+                default:
+                    break;
+
+            }
+            output = Double.Parse(tB_output.Text);
+            label_operation_string.Text = "";
+
+        }
+
+    }
+}
